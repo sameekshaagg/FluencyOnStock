1e876815f204b2440213ba812492310c24628160
sameekshaagg <61094310+sameekshaagg@users.noreply.github.com>
2025-08-14 21:47:32 +0200

    Delete 0_import_requests.py

--- 0_import_requests.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# used to download the English word frequency list from Hugging Face
-# Login using e.g. `huggingface-cli login` to access this dataset
-df = pd.read_parquet("hf://datasets/g2p-exploration/english-word-frequency-list/data/train-00000-of-00001.parquet")
-df.to_csv("word_freq_big.csv", index=False)
